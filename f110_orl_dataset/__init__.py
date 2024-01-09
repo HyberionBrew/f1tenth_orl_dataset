d363a237a53e17ef4a027c2cceca3999a167b33e
Fabian Kresse <e11707724@student.tuwien.ac.at>
2024-01-09 17:11:00 +0100

    forgot init

--- f110_orl_dataset/__init__.py
+++ f110_orl_dataset/__init__.py
@@ -3,6 +3,14 @@ from .dataset_env import F1tenthDatasetEnv
 
 # This will override the existing registration#
 register(
-    id='f110_with_dataset-v0',
+    id='f110-real-v0',
     entry_point='f110_orl_dataset.dataset_env:F1tenthDatasetEnv',
+    kwargs = {'dataset_url': "https://github.com/HyberionBrew/f110_datasets/raw/main/dataset_real_v0.zip"}
+)
+
+register(
+    id='f110-sim-v0',  
+    entry_point='f110_orl_dataset.dataset_env:F1tenthDatasetEnv',
+    kwargs = {'dataset_url': "https://github.com/HyberionBrew/f110_datasets/raw/main/dataset_sim_v0.zip"}
+    # kwargs={'param1': value1, 'param2': value2, ...}
 )
